c0abab61cb0403a2bde1dd3d84dd4ec47c38460c
Enjoy3325 <enjoy3325@gmail.com>
2022-10-25 00:08:50 +0300

    add input /btn lead more

---
+++ src/index.js
@@ -0,0 +1,131 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import Notiflix from 'notiflix';
+
+import NewsApiService from './new-service';
+import './css/styles.css';
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+});
+
+const refs = {
+  searchForm: document.querySelector('.search-form'),
+  divGallery: document.querySelector('.gallery'),
+  buttonLoadMore: document.querySelector('.load-more'),
+};
+// Делаю экземпляр класса чтобы получить объект с методами и свойствами
+const newsApiService = new NewsApiService();
+console.log(refs.searchForm);
+refs.searchForm.addEventListener('submit', onSearch);
+refs.buttonLoadMore.addEventListener('click', onLoadMoreClick);
+
+// функция сабмита поисковая строка
+async function onSearch(e) {
+  e.preventDefault();
+  clearCards();
+
+  console.log(e.target.elements.searchQuery.value);
+  newsApiService.query = e.target.elements.searchQuery.value;
+
+  if (newsApiService.query === '') {
+    return Notiflix.Notify.info('Enter in the field what you want to find!');
+  }
+  newsApiService.resetPage();
+  // пошёл HTTP запрос
+  try {
+    // Вызов картинок
+    const resultFetch = await newsApiService.fetchHits();
+
+    if (resultFetch.data.hits.length === 0) {
+      refs.buttonLoadMore.classList.add('is-hidden');
+      Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+      return;
+    }
+    Notiflix.Notify.info(
+      `Hooray! We found ${resultFetch.data.totalHits} images.`
+    );
+
+    galleryImage(resultFetch.data.hits);
+    lightbox.refresh();
+    refs.buttonLoadMore.classList.remove('is-hidden');
+    if (resultFetch.data.totalHits <= 40) {
+      refs.buttonLoadMore.classList.add('is-hidden');
+      Notiflix.Notify.failure(
+        "We're sorry, but you've reached the end of search results."
+      );
+    }
+  } catch (error) {}
+}
+
+// Функция кнопка показать больше картинок
+async function onLoadMoreClick() {
+  newsApiService.incrementPage();
+  try {
+    const resultClik = await newsApiService.fetchHits();
+    if (
+      newsApiService.per_page * newsApiService.page >
+      resultClik.data.totalHits
+    ) {
+      Notiflix.Notify.failure(
+        "We're sorry, but you've reached the end of search results."
+      );
+      refs.buttonLoadMore.classList.add('is-hidden');
+    }
+
+    galleryImage(resultClik.data.hits);
+    // Галерея lightbox
+    lightbox.refresh();
+    onScroll();
+  } catch (error) {}
+}
+
+function clearCards() {
+  refs.divGallery.innerHTML = '';
+}
+// Функция разметки, рендеринг
+function galleryImage(search) {
+  const gallaryImageResult = search.map(
+    ({
+      largeImageURL,
+      webformatURL,
+      tags,
+      likes,
+      views,
+      comments,
+      downloads,
+    }) => {
+      return ` <a class="list" href=${largeImageURL}><div class="photo-card">
+   <img src=" ${webformatURL}" alt="${tags}" loading="lazy" />
+   <div class="info">
+     <p class="info-item">
+       <b>Likes ${likes}</b>
+     </p>
+     <p class="info-item">
+       <b>Views ${views}</b>
+     </p>
+     <p class="info-item">
+       <b>Comments ${comments}</b>
+     </p>
+     <p class="info-item">
+       <b>Downloads  ${downloads}</b>
+     </p>
+   </div>
+ </div></a>`;
+    }
+  );
+  refs.divGallery.insertAdjacentHTML('beforeend', gallaryImageResult.join(''));
+}
+// Скролл
+function onScroll() {
+  const { height: cardHeight } = document
+    .querySelector('.gallery')
+    .firstElementChild.getBoundingClientRect();
+
+  window.scrollBy({
+    top: cardHeight * 2,
+    behavior: 'smooth',
+  });
+}
